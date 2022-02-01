43bbc00eeb08bcd14fdcfd09ca4c9cf8fef63d2e
Emre Kara <emrekara.dev@gmail.com>
2022-02-02 00:30:38 +0300

    404 not found page added

--- pages/404.jsx
+++ pages/404.jsx
@@ -7,9 +7,9 @@ const NotFound = () => {
     <>
       <div className="h-screen flex flex-col items-center">
         <Header />
-        <div className="bg-black h-full w-full flex flex-col items-center justify-center">
-          <h1 className="text-white text-5xl font-bold">404 Not Found!</h1>
-          <button className="text-white p-4 mt-5 border hover:bg-white hover:text-black">
+        <div className=" h-full w-full flex flex-col items-center justify-center">
+          <h1 className="text-5xl font-bold">404 Not Found!</h1>
+          <button className="p-4 mt-5 border border-black hover:bg-black hover:text-white">
             <Link href="/">
               <a>Return to Home Page</a>
             </Link>
