4afec3c5846885a205f23953396dd3fee646ff48
Emre Kara <emrekara.dev@gmail.com>
2022-02-03 22:15:44 +0300

    meta tag add

--- components/Helpers/Head.jsx
+++ components/Helpers/Head.jsx
@@ -4,6 +4,7 @@ const Head = ({ title, desc }) => {
     <>
       <title>{title}</title>
       <meta name="description" content={`${desc}`} />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com"  />
